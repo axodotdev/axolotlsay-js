ca39e5de8e3b5fbfe31e0fdef181994ea6ab7aea
Ashley Williams <ashley666ashley@gmail.com>
2023-11-15 12:42:24 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+#! /usr/bin/env node
+
+const yargs = require('yargs/yargs');
+const { hideBin } = require('yargs/helpers');
+const argv = yargs(hideBin(process.argv)).argv;
+
+const msg = argv.msg;
+console.log(msg);
