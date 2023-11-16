a76fef9e24f596ef58eab42e07609f0a9fc0ca12
Misty De Méo <misty@axo.dev>
2023-11-16 13:34:43 -0800

    feat: add build script

---
+++ build.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+set -euo pipefail
+
+EXE_BASENAME=axolotlsay-js
+NODE_VERSION=node18
+
+npm install
+
+args=
+
+if [ -n "${CARGO_DIST_TARGET:-}" ]; then
+    case "${CARGO_DIST_TARGET}" in
+        x86_64-pc-windows-msvc)
+            args="--targets ${NODE_VERSION}-win-x86_64 --output ${EXE_BASENAME}.exe"
+            ;;
+        aarch64-apple-darwin)
+            args="--targets ${NODE_VERSION}-darwin-arm64 --output ${EXE_BASENAME}"
+            ;;
+        x86_64-apple-darwin)
+            args="--targets ${NODE_VERSION}-darwin-x86_64 --output ${EXE_BASENAME}"
+            ;;
+        aarch64-linux-gnu)
+            args="--targets ${NODE_VERSION}-linux-arm64 --output ${EXE_BASENAME}"
+            ;;
+        x86_64-linux-gnu)
+            args="--targets ${NODE_VERSION}-linux-x86_64 --output ${EXE_BASENAME}"
+            ;;
+        *)
+            echo "Platform not supported: ${CARGO_DIST_TARGET}"
+            exit 1
+        esac
+fi
+
+npm run package -- $args
